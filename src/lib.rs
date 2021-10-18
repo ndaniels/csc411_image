45c12f6732f33be0356c82d8dc68740c750f3b9d
Najib Ishaq <najib.ishaq@axleinfo.com>
2021-10-18 12:01:21 -0400

    removed useless test

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,3 @@ mod image;
 mod pixel;
 pub use crate::image::Image;
 pub use crate::pixel::{Gray, Pixel, Rgb};
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
